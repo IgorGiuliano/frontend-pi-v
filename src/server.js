2dbf9ed7bc860b72b212fd33cafc634afe9c395e
IgorGiuliano <igor.andrade07@gmail.com>
2022-05-30 17:58:46 -0300

    make it

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(PORT);
