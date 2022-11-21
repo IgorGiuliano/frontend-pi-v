af19d98ae946607d6806ef75d6daf47ed2715eee
Igor Giuliano <49456490+IgorGiuliano@users.noreply.github.com>
2022-11-20 22:26:19 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
-    // baseURL: 'https://apicacamba.herokuapp.com'
+    //baseURL: 'http://localhost:5000'
+    baseURL: 'https://apicacamba.herokuapp.com'
 });
 
 export default api;
